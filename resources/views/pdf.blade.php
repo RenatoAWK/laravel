7bf2965ad4966737fbe644d80f29e63168a637b9
Renato Ferreira <Renatoawk@gmail.com>
2020-10-10 22:10:32 -0300

    Gerando PDF

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -28,9 +28,36 @@
                         </tbody>
                     </table>
                 </div>
+                <button class="btn btn-primary" onclick="generatePDF('{{$order->id}}', '{{$order->items}}')">
+                    Generate order PDF file
+                </button>
             </div>
             @endforeach
         </div>
     </div>
 </div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.4.1/jspdf.debug.js" integrity="sha384-THVO/sM0mFD9h7dfSndI6TS0PgAGavwKvB5hAxRRvc0o9cPLohB0wb/PTA7LdUHs" crossorigin="anonymous"></script>
+<script type="text/javascript">
+    function generatePDF(id, items) {
+        console.log('chamou')
+        const doc = jsPDF();
+        start = 90;
+        doc.setFontSize(30);
+        doc.text(30, 30, 'Order ' + id)
+        doc.setFontSize(10);
+        objList = JSON.parse(items);
+        console.log(objList);
+        objList.forEach(function(objItem) {
+            doc.text(30, start, "ID");
+            doc.text(60, start, String(objItem.id));
+            start += 10;
+            doc.text(30, start, "Price");
+            doc.text(60, start, String(objItem.price));
+            start += 25;
+        })
+
+        doc.save("order.pdf")
+
+    };
+</script>
 @endsection
